831c5ec4723be875b61fe5ba5bb3cd53c37ca66a
Seth <scheibs@mail.gvsu.edu>
2016-04-06 19:25:17 -0400

    filled splitImage with declaration

--- app/src/main/java/edu/gvsu/cis/kernohad/scrimage/Cell.java
+++ app/src/main/java/edu/gvsu/cis/kernohad/scrimage/Cell.java
@@ -8,7 +8,11 @@ import android.graphics.Bitmap;
 
 public class Cell implements Comparable<Cell> {
     public int row, column, value;
-    private Bitmap img;
+    Bitmap orig, bm1, bm2, bm3, bm4,
+                         bm5, bm6, bm7, bm8,
+                         bm9, bm10, bm11, bm12,
+                         bm13, bm14, bm15;//bm16 empty
+
 
     public Cell()
     {
@@ -34,7 +38,36 @@ public class Cell implements Comparable<Cell> {
         return this.column - other.column;
     }
 
-    public void drawPiece(Bitmap img, int i){
+    public void splitImage(){
+        int wSub = orig.getWidth()/4;
+        int hSub = orig.getHeight()/4;
 
+        //bitmap factory image
+        //1st row
+        bm1 = Bitmap.createBitmap(orig,      0, 0,   wSub, hSub);
+        bm2 = Bitmap.createBitmap(orig,   wSub, 0, 2*wSub, hSub);
+        bm3 = Bitmap.createBitmap(orig, 2*wSub, 0, 3*wSub, hSub);
+        bm4 = Bitmap.createBitmap(orig, 3*wSub, 0, 4*wSub, hSub);
+
+        //2nd row
+        bm5 = Bitmap.createBitmap(orig,      0, hSub,   wSub, 2*hSub);
+        bm6 = Bitmap.createBitmap(orig,   wSub, hSub, 2*wSub, 2*hSub);
+        bm7 = Bitmap.createBitmap(orig, 2*wSub, hSub, 3*wSub, 2*hSub);
+        bm8 = Bitmap.createBitmap(orig, 3*wSub, hSub, 4*wSub, 2*hSub);
+
+        //3rd row
+        bm9  = Bitmap.createBitmap(orig,      0, 2*hSub,   wSub, 3*hSub);
+        bm10 = Bitmap.createBitmap(orig,   wSub, 2*hSub, 2*wSub, 3*hSub);
+        bm11 = Bitmap.createBitmap(orig, 2*wSub, 2*hSub, 3*wSub, 3*hSub);
+        bm12 = Bitmap.createBitmap(orig, 3*wSub, 2*hSub, 4*wSub, 3*hSub);
+
+        //4th row
+        bm13 = Bitmap.createBitmap(orig,      0, 3*hSub,   wSub, 4*hSub);
+        bm14 = Bitmap.createBitmap(orig,   wSub, 3*hSub, 2*wSub, 4*hSub);
+        bm15 = Bitmap.createBitmap(orig, 2*wSub, 3*hSub, 3*wSub, 4*hSub);
     }
 }
+//        orig,      0, 0,   wSub, hSub);
+//        orig,   wSub, 0, 2*wSub, hSub);
+//        orig, 2*wSub, 0, 3*wSub, hSub);
+//        orig, 3*wSub, 0, 4*wSub, hSub);
