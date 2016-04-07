a07125e70eba470c27d7bf3f387d4ad3bb7da6d7
Seth <scheibs@mail.gvsu.edu>
2016-04-07 15:45:17 -0400

    Cell methods now return Bitmaps

--- app/src/main/java/edu/gvsu/cis/kernohad/scrimage/Cell.java
+++ app/src/main/java/edu/gvsu/cis/kernohad/scrimage/Cell.java
@@ -53,48 +53,55 @@ public class Cell implements Comparable<Cell> {
         return metrics.widthPixels;
     }
 
-    public void splitImage(){
+    public Bitmap splitImage(int i){
         int wSub, hSub, x = getDisplayWidth();
         String w = ""+x;
         String url = "https://unsplash.it/"+w+"/"+w+"/?random";
-        getBitmapFromURL(url);
-        wSub = orig.getWidth()/4;
-        hSub = orig.getHeight()/4;
+        if(getBitmapFromURL(url) == null)
+            Log.d("Error", "orig Bitmap is null");
+        else {
+            orig = getBitmapFromURL(url);
+            wSub = orig.getWidth() / 4;
+            hSub = orig.getHeight() / 4;
+            switch (i) {
+                //1st row
+                case 0: return Bitmap.createBitmap(orig, 0, 0, wSub, hSub);
+                case 1: return Bitmap.createBitmap(orig, wSub, 0, 2 * wSub, hSub);
+                case 2: return Bitmap.createBitmap(orig, 2 * wSub, 0, 3 * wSub, hSub);
+                case 3: return Bitmap.createBitmap(orig, 3 * wSub, 0, 4 * wSub, hSub);
 
-        //1st row
-        bm1 = Bitmap.createBitmap(orig,      0, 0,   wSub, hSub);
-        bm2 = Bitmap.createBitmap(orig,   wSub, 0, 2*wSub, hSub);
-        bm3 = Bitmap.createBitmap(orig, 2*wSub, 0, 3*wSub, hSub);
-        bm4 = Bitmap.createBitmap(orig, 3*wSub, 0, 4*wSub, hSub);
+                //2nd row
+                case 4: return Bitmap.createBitmap(orig, 0, hSub, wSub, 2 * hSub);
+                case 5: return Bitmap.createBitmap(orig, wSub, hSub, 2 * wSub, 2 * hSub);
+                case 6: return Bitmap.createBitmap(orig, 2 * wSub, hSub, 3 * wSub, 2 * hSub);
+                case 7: return Bitmap.createBitmap(orig, 3 * wSub, hSub, 4 * wSub, 2 * hSub);
 
-        //2nd row
-        bm5 = Bitmap.createBitmap(orig,      0, hSub,   wSub, 2*hSub);
-        bm6 = Bitmap.createBitmap(orig,   wSub, hSub, 2*wSub, 2*hSub);
-        bm7 = Bitmap.createBitmap(orig, 2*wSub, hSub, 3*wSub, 2*hSub);
-        bm8 = Bitmap.createBitmap(orig, 3*wSub, hSub, 4*wSub, 2*hSub);
+                //3rd row
+                case 8:  return Bitmap.createBitmap(orig, 0, 2 * hSub, wSub, 3 * hSub);
+                case 9:  return Bitmap.createBitmap(orig, wSub, 2 * hSub, 2 * wSub, 3 * hSub);
+                case 10: return Bitmap.createBitmap(orig, 2 * wSub, 2 * hSub, 3 * wSub, 3 * hSub);
+                case 11: return Bitmap.createBitmap(orig, 3 * wSub, 2 * hSub, 4 * wSub, 3 * hSub);
 
-        //3rd row
-        bm9  = Bitmap.createBitmap(orig,      0, 2*hSub,   wSub, 3*hSub);
-        bm10 = Bitmap.createBitmap(orig,   wSub, 2*hSub, 2*wSub, 3*hSub);
-        bm11 = Bitmap.createBitmap(orig, 2*wSub, 2*hSub, 3*wSub, 3*hSub);
-        bm12 = Bitmap.createBitmap(orig, 3*wSub, 2*hSub, 4*wSub, 3*hSub);
-
-        //4th row
-        bm13 = Bitmap.createBitmap(orig,      0, 3*hSub,   wSub, 4*hSub);
-        bm14 = Bitmap.createBitmap(orig,   wSub, 3*hSub, 2*wSub, 4*hSub);
-        bm15 = Bitmap.createBitmap(orig, 2*wSub, 3*hSub, 3*wSub, 4*hSub);
+                //4th row
+                case 12: return Bitmap.createBitmap(orig, 0, 3 * hSub, wSub, 4 * hSub);
+                case 13: return Bitmap.createBitmap(orig, wSub, 3 * hSub, 2 * wSub, 4 * hSub);
+                case 14: return Bitmap.createBitmap(orig, 2 * wSub, 3 * hSub, 3 * wSub, 4 * hSub);
+            }
+        }
+        return null;
     }
 
-    public void getBitmapFromURL(String src){
+    public Bitmap getBitmapFromURL(String src){
         try {
             URL url = new URL(src);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setDoInput(true);
             con.connect();
             InputStream input = con.getInputStream();
-            orig = BitmapFactory.decodeStream(input);
+            return BitmapFactory.decodeStream(input);
         } catch (IOException e) {
             Log.d("IOException","IOException in getBitmapFromURL");
+            return null;
         }
     }
 }
