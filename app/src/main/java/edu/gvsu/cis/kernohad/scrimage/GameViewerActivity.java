38e4d617bf6f909db93dadee452997f4f567f099
Seth <scheibs@mail.gvsu.edu>
2016-04-12 17:43:13 -0400

    splitImage insatiantes bm's

--- app/src/main/java/edu/gvsu/cis/kernohad/scrimage/GameViewerActivity.java
+++ app/src/main/java/edu/gvsu/cis/kernohad/scrimage/GameViewerActivity.java
@@ -27,7 +27,10 @@ public class GameViewerActivity extends AppCompatActivity implements GestureDete
     GestureDetectorCompat gDetector;
     IPresenter  presenter;
     TextView[][] tvArray;
-    Bitmap orig;
+    Bitmap orig, bm0, bm1, bm2, bm3,
+                 bm4, bm5, bm6, bm7,
+                 bm8, bm9, bm10, bm11,
+                 bm12, bm13, bm14;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -166,6 +169,11 @@ public class GameViewerActivity extends AppCompatActivity implements GestureDete
                     tvArray[k][m].setText("");
     }
 
+    /*
+    for(int i=0, i<15;i++){
+        splitImage(orig, i);
+     */
+
     @Override
     public void showMessage(String msg) {
 
@@ -176,7 +184,7 @@ public class GameViewerActivity extends AppCompatActivity implements GestureDete
         return metrics.widthPixels;
     }
 
-    public Bitmap splitImage(ImageView img, int i)
+    public void splitImage(Bitmap img)
             /*
             parameter: an int to tell the function which bitmap to return.
             Sugg.:  Use a loop from 0 to 14 to "paint" each returned bitmap to
@@ -184,35 +192,27 @@ public class GameViewerActivity extends AppCompatActivity implements GestureDete
             */
     {
         int wSub, hSub;
-        orig = getBitmapFromImageView(img);
+        orig = img;
         wSub = orig.getWidth() / 4;
         hSub = orig.getHeight() / 4;
-        switch (i) {
-            //1st row
-            case 0: return Bitmap.createBitmap(orig, 0, 0, wSub, hSub);
-            case 1: return Bitmap.createBitmap(orig, wSub, 0, 2 * wSub, hSub);
-            case 2: return Bitmap.createBitmap(orig, 2 * wSub, 0, 3 * wSub, hSub);
-            case 3: return Bitmap.createBitmap(orig, 3 * wSub, 0, 4 * wSub, hSub);
-            //2nd row
-            case 4: return Bitmap.createBitmap(orig, 0, hSub, wSub, 2 * hSub);
-            case 5: return Bitmap.createBitmap(orig, wSub, hSub, 2 * wSub, 2 * hSub);
-            case 6: return Bitmap.createBitmap(orig, 2 * wSub, hSub, 3 * wSub, 2 * hSub);
-            case 7: return Bitmap.createBitmap(orig, 3 * wSub, hSub, 4 * wSub, 2 * hSub);
-            //3rd roW
-            case 8:  return Bitmap.createBitmap(orig, 0, 2 * hSub, wSub, 3 * hSub);
-            case 9:  return Bitmap.createBitmap(orig, wSub, 2 * hSub, 2 * wSub, 3 * hSub);
-            case 10: return Bitmap.createBitmap(orig, 2 * wSub, 2 * hSub, 3 * wSub, 3 * hSub);
-            case 11: return Bitmap.createBitmap(orig, 3 * wSub, 2 * hSub, 4 * wSub, 3 * hSub);
-            //4th row
-            case 12: return Bitmap.createBitmap(orig, 0, 3 * hSub, wSub, 4 * hSub);
-            case 13: return Bitmap.createBitmap(orig, wSub, 3 * hSub, 2 * wSub, 4 * hSub);
-            case 14: return Bitmap.createBitmap(orig, 2 * wSub, 3 * hSub, 3 * wSub, 4 * hSub);
-        }
-        return null;
-    }
-
-    public Bitmap getBitmapFromImageView(ImageView img){
-        img.buildDrawingCache();
-        return img.getDrawingCache();
+        //1st row
+        bm0  = Bitmap.createBitmap(orig, 0, 0, wSub, hSub);
+        bm1  = Bitmap.createBitmap(orig, wSub, 0, 2 * wSub, hSub);
+        bm2  = Bitmap.createBitmap(orig, 2 * wSub, 0, 3 * wSub, hSub);
+        bm3  = Bitmap.createBitmap(orig, 3 * wSub, 0, 4 * wSub, hSub);
+        //2nd row
+        bm4  = Bitmap.createBitmap(orig, 0, hSub, wSub, 2 * hSub);
+        bm5  = Bitmap.createBitmap(orig, wSub, hSub, 2 * wSub, 2 * hSub);
+        bm6  = Bitmap.createBitmap(orig, 2 * wSub, hSub, 3 * wSub, 2 * hSub);
+        bm7  = Bitmap.createBitmap(orig, 3 * wSub, hSub, 4 * wSub, 2 * hSub);
+        //3rd roW
+        bm8  = Bitmap.createBitmap(orig, 0, 2 * hSub, wSub, 3 * hSub);
+        bm9  = Bitmap.createBitmap(orig, wSub, 2 * hSub, 2 * wSub, 3 * hSub);
+        bm10 = Bitmap.createBitmap(orig, 2 * wSub, 2 * hSub, 3 * wSub, 3 * hSub);
+        bm11 = Bitmap.createBitmap(orig, 3 * wSub, 2 * hSub, 4 * wSub, 3 * hSub);
+        //4th row
+        bm12 = Bitmap.createBitmap(orig, 0, 3 * hSub, wSub, 4 * hSub);
+        bm13 = Bitmap.createBitmap(orig, wSub, 3 * hSub, 2 * wSub, 4 * hSub);
+        bm14 = Bitmap.createBitmap(orig, 2 * wSub, 3 * hSub, 3 * wSub, 4 * hSub);
     }
 }
