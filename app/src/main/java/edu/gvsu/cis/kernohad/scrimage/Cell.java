1c7e034c1b09410bba1e9a55d1a3c1c28d16af56
Seth <scheibs@mail.gvsu.edu>
2016-04-12 17:30:47 -0400

    bitapp images moved from Cell to GameViewer

--- app/src/main/java/edu/gvsu/cis/kernohad/scrimage/Cell.java
+++ app/src/main/java/edu/gvsu/cis/kernohad/scrimage/Cell.java
@@ -10,7 +10,6 @@ import android.widget.ImageView;
 
 public class Cell implements Comparable<Cell> {
     public int row, column, value;
-    public Bitmap orig;
 
 
     public Cell()
@@ -35,51 +34,6 @@ public class Cell implements Comparable<Cell> {
 
         /* break the tie using column */
         return this.column - other.column;
-    }
-
-    public int getDisplayWidth(){
-        DisplayMetrics metrics = new DisplayMetrics();
-        return metrics.widthPixels;
-    }
-
-    public Bitmap splitImage(ImageView img, int i)
-            /*
-            parameter: an int to tell the function which bitmap to return.
-            Sugg.:  Use a loop from 0 to 14 to "paint" each returned bitmap to
-                    the 2d array of ImageView.
-            */
-    {
-        int wSub, hSub;
-        orig = getBitmapFromImageView(img);
-        wSub = orig.getWidth() / 4;
-        hSub = orig.getHeight() / 4;
-        switch (i) {
-            //1st row
-            case 0: return Bitmap.createBitmap(orig, 0, 0, wSub, hSub);
-            case 1: return Bitmap.createBitmap(orig, wSub, 0, 2 * wSub, hSub);
-            case 2: return Bitmap.createBitmap(orig, 2 * wSub, 0, 3 * wSub, hSub);
-            case 3: return Bitmap.createBitmap(orig, 3 * wSub, 0, 4 * wSub, hSub);
-            //2nd row
-            case 4: return Bitmap.createBitmap(orig, 0, hSub, wSub, 2 * hSub);
-            case 5: return Bitmap.createBitmap(orig, wSub, hSub, 2 * wSub, 2 * hSub);
-            case 6: return Bitmap.createBitmap(orig, 2 * wSub, hSub, 3 * wSub, 2 * hSub);
-            case 7: return Bitmap.createBitmap(orig, 3 * wSub, hSub, 4 * wSub, 2 * hSub);
-            //3rd roW
-            case 8:  return Bitmap.createBitmap(orig, 0, 2 * hSub, wSub, 3 * hSub);
-            case 9:  return Bitmap.createBitmap(orig, wSub, 2 * hSub, 2 * wSub, 3 * hSub);
-            case 10: return Bitmap.createBitmap(orig, 2 * wSub, 2 * hSub, 3 * wSub, 3 * hSub);
-            case 11: return Bitmap.createBitmap(orig, 3 * wSub, 2 * hSub, 4 * wSub, 3 * hSub);
-            //4th row
-            case 12: return Bitmap.createBitmap(orig, 0, 3 * hSub, wSub, 4 * hSub);
-            case 13: return Bitmap.createBitmap(orig, wSub, 3 * hSub, 2 * wSub, 4 * hSub);
-            case 14: return Bitmap.createBitmap(orig, 2 * wSub, 3 * hSub, 3 * wSub, 4 * hSub);
-        }
-        return null;
-    }
-
-    public Bitmap getBitmapFromImageView(ImageView img){
-        img.buildDrawingCache();
-        return img.getDrawingCache();
     }
 }
 //        orig,      0, 0,   wSub, hSub);
--- app/src/main/java/edu/gvsu/cis/kernohad/scrimage/GameViewerActivity.java
+++ app/src/main/java/edu/gvsu/cis/kernohad/scrimage/GameViewerActivity.java
@@ -1,6 +1,7 @@
 package edu.gvsu.cis.kernohad.scrimage;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Gravity;
@@ -16,6 +18,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GameViewerActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, IView{
@@ -24,6 +27,7 @@ public class GameViewerActivity extends AppCompatActivity implements GestureDete
     GestureDetectorCompat gDetector;
     IPresenter  presenter;
     TextView[][] tvArray;
+    Bitmap orig;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -165,5 +169,50 @@ public class GameViewerActivity extends AppCompatActivity implements GestureDete
     @Override
     public void showMessage(String msg) {
 
+    }
+
+    public int getDisplayWidth(){
+        DisplayMetrics metrics = new DisplayMetrics();
+        return metrics.widthPixels;
+    }
+
+    public Bitmap splitImage(ImageView img, int i)
+            /*
+            parameter: an int to tell the function which bitmap to return.
+            Sugg.:  Use a loop from 0 to 14 to "paint" each returned bitmap to
+                    the 2d array of ImageView.
+            */
+    {
+        int wSub, hSub;
+        orig = getBitmapFromImageView(img);
+        wSub = orig.getWidth() / 4;
+        hSub = orig.getHeight() / 4;
+        switch (i) {
+            //1st row
+            case 0: return Bitmap.createBitmap(orig, 0, 0, wSub, hSub);
+            case 1: return Bitmap.createBitmap(orig, wSub, 0, 2 * wSub, hSub);
+            case 2: return Bitmap.createBitmap(orig, 2 * wSub, 0, 3 * wSub, hSub);
+            case 3: return Bitmap.createBitmap(orig, 3 * wSub, 0, 4 * wSub, hSub);
+            //2nd row
+            case 4: return Bitmap.createBitmap(orig, 0, hSub, wSub, 2 * hSub);
+            case 5: return Bitmap.createBitmap(orig, wSub, hSub, 2 * wSub, 2 * hSub);
+            case 6: return Bitmap.createBitmap(orig, 2 * wSub, hSub, 3 * wSub, 2 * hSub);
+            case 7: return Bitmap.createBitmap(orig, 3 * wSub, hSub, 4 * wSub, 2 * hSub);
+            //3rd roW
+            case 8:  return Bitmap.createBitmap(orig, 0, 2 * hSub, wSub, 3 * hSub);
+            case 9:  return Bitmap.createBitmap(orig, wSub, 2 * hSub, 2 * wSub, 3 * hSub);
+            case 10: return Bitmap.createBitmap(orig, 2 * wSub, 2 * hSub, 3 * wSub, 3 * hSub);
+            case 11: return Bitmap.createBitmap(orig, 3 * wSub, 2 * hSub, 4 * wSub, 3 * hSub);
+            //4th row
+            case 12: return Bitmap.createBitmap(orig, 0, 3 * hSub, wSub, 4 * hSub);
+            case 13: return Bitmap.createBitmap(orig, wSub, 3 * hSub, 2 * wSub, 4 * hSub);
+            case 14: return Bitmap.createBitmap(orig, 2 * wSub, 3 * hSub, 3 * wSub, 4 * hSub);
+        }
+        return null;
+    }
+
+    public Bitmap getBitmapFromImageView(ImageView img){
+        img.buildDrawingCache();
+        return img.getDrawingCache();
     }
 }
