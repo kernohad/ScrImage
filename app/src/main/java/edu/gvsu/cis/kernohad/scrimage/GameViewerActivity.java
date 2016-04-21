492b2281ed30effd982cee0c3da30b61841111ad
kernohad <kernohad@mail.gvsu.edu>
2016-04-21 02:12:58 -0400

    Little popup fix

--- app/src/main/java/edu/gvsu/cis/kernohad/scrimage/GameViewerActivity.java
+++ app/src/main/java/edu/gvsu/cis/kernohad/scrimage/GameViewerActivity.java
@@ -107,6 +107,7 @@ public class GameViewerActivity extends AppCompatActivity implements GestureDete
         popup = hold;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
